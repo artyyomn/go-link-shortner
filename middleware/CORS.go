d0ab4e500e6559f44890faf53b9d83cff472a1f0
akash <alfaxgem@gmail.com>
2026-03-15 13:17:29 +0530

    changed cors from localhost to site name

--- middleware/CORS.go
+++ middleware/CORS.go
@@ -6,7 +6,7 @@ import(
 
 func CORS(next http.Handler) http.Handler{
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request){
-		w.Header().Set("Access-Control-Allow-Origin", "http://localhost:6767/")
+		w.Header().Set("Access-Control-Allow-Origin", "https://go-link-shortner.onrender.com/")
 		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
 		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
 
